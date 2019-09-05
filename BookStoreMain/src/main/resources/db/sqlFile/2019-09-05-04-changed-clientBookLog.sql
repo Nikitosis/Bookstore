f6b681d0a9200d13d43494e4ee793937e17a6a8a
Nikita Pupov <nikitospupov@gmail.com>
2019-09-05 15:18:45 +0300

    Updated client_book_log db table. Changed ClientBookLogger server port

---
+++ BookStoreMain/src/main/resources/db/sqlFile/2019-09-05-04-changed-clientBookLog.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS client_book_log;
+
+CREATE TABLE bookstore.client_book_log
+(
+    id       INT          AUTO_INCREMENT,
+    client_id INT         NOT NULL,
+    book_id   INT         NOT NULL,
+    log_date  DATE        NOT NULL,
+    log_action ENUM('take','return') NOT NULL,
+    CONSTRAINT client_book_log_pk PRIMARY KEY (id),
+    CONSTRAINT FK_client FOREIGN KEY (client_id) REFERENCES clients (id),
+    CONSTRAINT FK_book FOREIGN KEY (book_id) REFERENCES books (id)
+);
