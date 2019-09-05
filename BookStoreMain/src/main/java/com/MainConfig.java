69163f400c101bef3e4ee45cf13bf9d88e104364
Nikita Pupov <nikitospupov@gmail.com>
2019-09-05 12:13:29 +0300

    Added DependencyService to BookStoreMain configuration(to obtain clientBookLogger's url from configuration file)

--- BookStoreMain/src/main/java/com/MainConfig.java
+++ BookStoreMain/src/main/java/com/MainConfig.java
@@ -1,5 +1,6 @@
 package com;
 
+import com.configurations.DependencyService;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 
@@ -8,6 +9,16 @@ import java.util.Map;
 public class MainConfig extends Configuration {
 
     private DataSourceFactory database;
+
+    private DependencyService clientBookLoggerService;
+
+    public DependencyService getClientBookLoggerService() {
+        return clientBookLoggerService;
+    }
+
+    public void setClientBookLoggerService(DependencyService clientBookLoggerService) {
+        this.clientBookLoggerService = clientBookLoggerService;
+    }
 
     public DataSourceFactory getDatabase() {
         return database;
