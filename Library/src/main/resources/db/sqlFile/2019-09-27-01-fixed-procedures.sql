527f2debf448fc9f5bb448458019ae9f165f2942
Nikita Pupov <nikitospupov@gmail.com>
2019-09-27 14:11:18 +0300

    fixed deleteUser/deleteBook sql procedures

---
+++ Library/src/main/resources/db/sqlFile/2019-09-27-01-fixed-procedures.sql
@@ -0,0 +1,25 @@
+DROP PROCEDURE IF EXISTS deleteUser//
+DROP PROCEDURE IF EXISTS deleteBook//
+
+CREATE PROCEDURE deleteUser(IN userId INT)
+    BEGIN
+        START TRANSACTION;
+
+        DELETE FROM bookstore.user_role WHERE user_id=userId;
+        DELETE FROM bookstore.user_book_log WHERE user_id=userId;
+        DELETE FROM bookstore.user_book WHERE user_id=userId;
+        DELETE FROM bookstore.users WHERE id=userId;
+
+        COMMIT;
+    END //
+
+CREATE PROCEDURE deleteBook(IN bookId INT)
+    BEGIN
+        START TRANSACTION;
+
+        DELETE FROM bookstore.user_book WHERE book_id=bookId;
+        DELETE FROM bookstore.user_book_log WHERE book_id=bookId;
+        DELETE FROM bookstore.books WHERE id=bookId;
+
+        COMMIT;
+    END //
