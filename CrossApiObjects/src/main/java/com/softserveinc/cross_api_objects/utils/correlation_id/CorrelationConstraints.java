f9ead4eb4ed9f501c861b445c363d070d2b9379e
Nikita Pupov <nikitospupov@gmail.com>
2019-11-14 15:00:52 +0200

    Removed ClientCorrelationInterceptor, because it cannot load correlationId from MDC. Moved constraints to CorrelationConstraints.

---
+++ CrossApiObjects/src/main/java/com/softserveinc/cross_api_objects/utils/correlation_id/CorrelationConstraints.java
@@ -0,0 +1,7 @@
+package com.softserveinc.cross_api_objects.utils.correlation_id;
+
+public class CorrelationConstraints {
+    public static final String CORRELATION_ID_HEADER_NAME ="X-Correlation-Id";
+    public static final String CORRELATION_ID_LOG_VAR_NAME ="correlationId";
+
+}
