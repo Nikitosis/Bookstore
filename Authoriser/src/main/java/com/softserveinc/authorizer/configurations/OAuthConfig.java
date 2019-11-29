f063b243329627799e421f9b57e34ebb21dc4484
Nikita Pupov <nikitospupov@gmail.com>
2019-11-29 15:47:41 +0200

    Reduced amount of parameters in OAuthConfig. Currently only clientId and clientSecret are required. Use env variables in configuration.yml

--- Authoriser/src/main/java/com/softserveinc/authorizer/configurations/OAuthConfig.java
+++ Authoriser/src/main/java/com/softserveinc/authorizer/configurations/OAuthConfig.java
@@ -3,11 +3,6 @@ package com.softserveinc.authorizer.configurations;
 public class OAuthConfig {
     private String clientId;
     private String clientSecret;
-    private String authorisationUri;
-    private String tokenUri;
-    private String userInfoUri;
-    private String scope;
-    private String redirectUriTemplate;
 
     public String getClientId() {
         return clientId;
@@ -23,45 +18,5 @@ public class OAuthConfig {
 
     public void setClientSecret(String clientSecret) {
         this.clientSecret = clientSecret;
-    }
-
-    public String getAuthorisationUri() {
-        return authorisationUri;
-    }
-
-    public void setAuthorisationUri(String authorisationUri) {
-        this.authorisationUri = authorisationUri;
-    }
-
-    public String getTokenUri() {
-        return tokenUri;
-    }
-
-    public void setTokenUri(String tokenUri) {
-        this.tokenUri = tokenUri;
-    }
-
-    public String getUserInfoUri() {
-        return userInfoUri;
-    }
-
-    public void setUserInfoUri(String userInfoUri) {
-        this.userInfoUri = userInfoUri;
-    }
-
-    public String getScope() {
-        return scope;
-    }
-
-    public void setScope(String scope) {
-        this.scope = scope;
-    }
-
-    public String getRedirectUriTemplate() {
-        return redirectUriTemplate;
-    }
-
-    public void setRedirectUriTemplate(String redirectUriTemplate) {
-        this.redirectUriTemplate = redirectUriTemplate;
     }
 }
